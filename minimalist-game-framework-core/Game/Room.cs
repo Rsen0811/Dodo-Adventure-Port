af7c2a27f7d32e13cbcd885aedd2abcd654c6ae5
SENGUPTA <s-rasengupta@lwsd.org>
2022-11-15 16:03:24 -0800

    testing again to see if commits

---
+++ minimalist-game-framework-core/Game/Room.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Room
+{
+    Vector2 PLAYER_SIZE = new Vector2(24, 24);
+    List<Bounds2> CollisionZones;
+    
+    public Room()
+    {
+        CollisionZones = new List<Bounds2>();
+        CollisionZones.Add(new Bounds2(new Vector2(100, 200), new Vector2(150, 200)));
+    }
+
+    public void Update()
+    {
+    }
+
+    public Vector2 move(Vector2 start, Vector2 move)
+    {
+        Vector2 moveTo = start + move;
+        Bounds2 playerBounds = new Bounds2(new Vector2(start.X, start.X + PLAYER_SIZE.X),
+                                    new Vector2(start.Y, start.Y + PLAYER_SIZE.Y));
+
+
+        foreach (Bounds2 rect in CollisionZones)
+        { // position is actual x range and size is actually y range
+            if (checkIntervalIntersect(rect.Position, playerBounds.Position)) moveTo.X = 0;
+            if (checkIntervalIntersect(rect.Size, playerBounds.Size)) moveTo.Y = 0;
+        }
+
+        return moveTo;
+    }
+
+    private bool checkIntervalIntersect(Vector2 barrier, Vector2 player)
+    {
+        if (player.X < barrier.Y && player.X > barrier.X) return true;
+        if (player.Y < barrier.Y && player.Y > barrier.X) return true;    
+        if (barrier.X < player.Y && barrier.X > player.X) return true;
+        return false;
+    }
+
+    public void drawRoom()
+    {
+        Engine.DrawRectSolid(new Bounds2(new Vector2(100, 150), new Vector2(100, 50)), Color.White);
+    }
+
+    public void addObject()
+    {
+
+    }
+
+    public void removeObject()
+    {
+
+    }
+
+    public void pickup()
+    {
+
+    }
+}
+
