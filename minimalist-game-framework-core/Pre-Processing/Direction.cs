df0b6a8c08a31fde734246dc3f3ed54d57e61ccb
VIGNESH SRINIVASAN <s-vsrinivasan@lwsd.org>
2022-11-19 12:55:10 -0800

    -finished graph class -added a new folder -finished direction class

---
+++ minimalist-game-framework-core/Pre-Processing/Direction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+class Direction
+{
+    public int LEFT = 0;
+    public int RIGHT = 1;
+    public int DOWN = 2;
+    public int UP = 3;
+}
+
---
+++ minimalist-game-framework-core/Pre-Processing/Graph.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+class Graph
+{
+    //cols of the adjList represent directions
+    //0 is left
+    //1 is right
+    //2 is down
+    //3 is left
+    int[,] adjList;
+    public Graph(int V) {
+        this.adjList = new int[V, 4];
+        for(int i = 0; i < V; i++)
+        {
+            adjList[i, 0] = -1;
+            adjList[i, 1] = -1;
+            adjList[i, 2] = -1;
+            adjList[i, 3] = -1;
+        }
+    }
+    public int getAdj(int v,int dir)
+    {
+        return adjList[v, dir];
+    }
+}
+
