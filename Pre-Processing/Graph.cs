ebf3e89e9379f7dec1d09bee2140120ba4f1dca8
Vignesh <s-vsrinivasan@lwsd.org>
2022-11-21 00:24:32 -0800

    half of the DFS is complete -realized bug wont work with walls thicker that 1

--- Pre-Processing/Graph.cs
+++ Pre-Processing/Graph.cs
@@ -10,24 +10,38 @@ class Graph
     //3 is left
     int[,] adjList;
     public Graph(int V) {
-        this.adjList = new int[V, 4];
+        this.adjList = new int[V, 5];
         for(int i = 0; i < V; i++)
         {
             adjList[i, 0] = -1;
             adjList[i, 1] = -1;
             adjList[i, 2] = -1;
             adjList[i, 3] = -1;
+            adjList[i, 4] = -1;
         }
     }
     public int getAdj(int v,int dir)
     {
-        return adjList[v, dir];
+        try
+        {
+            return adjList[v, dir];
+        }catch(Exception e)
+        {
+            return -1;
+        }
     }
 
     public void addEdge(int v1, int v2, int dir)
     {
         adjList[v1, dir] = v2;
         adjList[v2, Direction.reverse(dir)] = v1;
+        adjList[v1,4] = 1;
+        adjList[v2, 4] = 1;
     }
+    public boolean doesExist(int v)
+    {
+        return adjList[v,4] == 1;
+    }
+
 }
 
--- Pre-Processing/RectangleHealing.cs
+++ Pre-Processing/RectangleHealing.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Schema;
+
 class RectangleHealing
 {
+    int rows=3;
+    int columns=3;
     public static void Main(String[] args)
     {
         int[,] collisionMap =
@@ -11,8 +15,7 @@ class RectangleHealing
             {1,0,1},
             {1,1,1}
         };
-        int rows=3;
-        int columns=3;
+        
         
         //adding map to the graph
         Graph map = new Graph(rows*columns);
@@ -37,10 +40,78 @@ class RectangleHealing
                     if (i > 0 && collisionMap[i, j+1] == 1)
                     {
                         map.addEdge(i * rows + j, i * rows + j+1, Direction.RIGHT);
-                    }
+                    }s
                 }
             }
         }
 
+        //DFS the graph weirdly
+        bool[] visited=new bool[rows*columns];
+        List<Rect> rects=new List<Rect>();
+                
     }
+    public static void DFS(Graph graph,int currNode, bool[] visited, List<Rect> rects, int currDir,Rect currRect)
+    {
+        if (graph.doesExist(currNode) && !visited[currNode])
+        {
+            visited[currNode]=true;
+            int sameDir= graph.getAdj(currNode,currDir)
+            if (sameDir != -1)){
+                Rect newRect;
+                int row =currNode/columns;
+                int col = currNode%columns;
+                if(currRect == null)
+                {    
+                    //assuming tile size is one
+                    newRect= new Rect(col-1,col,row,row+1);
+                    DFS(graph, sameDir, visited, rects, currDir, newRect);
+                }
+                else
+                {
+                    if (currDir = Direction.RIGHT)
+                    {
+                        newRect = new Rect(col - 1, currRect.xMax, row, row+1);
+                        DFS(graph, sameDir, visited, rects, currDir, newRect);
+                    }
+                    else if (currDir = Direction.LEFT)
+                    {
+                        newRect = new Rect(currRect.xMin, col, row, row + 1);
+                        DFS(graph, sameDir, visited, rects, currDir, newRect);
+                    }
+                    else if(currDir = Direction.UP)
+                    {
+                        newRect = new Rect(col - 1, col, row, currRect.yMax);
+                        DFS(graph, sameDir, visited, rects, currDir, newRect);
+                    }
+                    else if(currDir = Direction.DOWN)
+                    {
+                        newRect = new Rect(col - 1, col, currRect.yMin, row + 1);
+                        DFS(graph, sameDir, visited, rects, currDir, newRect);
+                    }
+                }
+            }
+        }
+        visited[currNode]=true;
+        for(int i = 0; i < visited.Length; i++)
+        {
+            if (!visited[i])
+            { 
+                return DFS(graph,i,visited,rects,-1,null);
+            }        
+        }
+    }
+    class Rect
+    {
+        public int xMin;
+        public int xMax;
+        public int yMin;
+        public int yMax;
+        public Rect(xMin, xMax, yMin, yMax)
+        {
+            this.xMax =xMax
+            this.yMin=yMin;
+            this.xMin=xMin;
+            this.yMax=xMax;
+        }
+     }
 }
