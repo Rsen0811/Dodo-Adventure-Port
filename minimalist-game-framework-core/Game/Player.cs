70a23c1ab6730bc7532f0b13636936e721fc71c4
rohan chidurala <s-rchidurala@lwsd.org>
2022-11-22 13:08:05 -0800

    Initial Player model

---
+++ minimalist-game-framework-core/Game/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Game.Game
+{
+    internal class Player
+    {
+        Vector2 pos;
+        bool active;
+        Item holding;
+        Vector2 currRoom;
+
+        public Vector2 move()
+        {
+            return Vector2.Zero;
+        }
+        public Item pickup()
+        {
+            return Item;
+        }
+        public Item drop()
+        {
+            return Item;
+        }
+
+        public Vector2 drawPlayer()
+        {
+            return Vector2.Zero;
+        }
+    }
+}
