d637547dbd126f5532610806afc1e643a96687b1
KESHAV ACHARYA <s-KACHARYA@lwsd.org>
2022-12-05 13:25:31 -0800

    basic items

---
+++ minimalist-game-framework-core/Game/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+interface Item
+{
+    void draw();
+
+    Rect collisionZone();
+
+    bool isHeld();
+
+    void Update();
+
+    bool collides(Rect bounds);
+
+
+
+}
---
+++ minimalist-game-framework-core/Game/Sword.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Sword : Item
+{
+    public bool isHoly;
+    public int damage;
+    private Texture spriteMap;
+    private Vector2 pos;
+    private Vector2 dir;
+
+
+    public Sword()
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+    public void draw()
+    {
+
+    }
+
+    public bool isHeld()
+    {
+        return true;
+    }
+
+    public bool collides(Rect player)
+    {
+        return true;
+    }
+
+    public bool dodoCollides()
+    {
+
+    }
+
+    public Rect collisionZone()
+    {
+        return null;
+    }
+}
