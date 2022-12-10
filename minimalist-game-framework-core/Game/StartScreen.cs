1b6b67af5db2a8c7f90cbba698d0a8aa5dc5186d
VIGNESH SRINIVASAN <s-vsrinivasan@lwsd.org>
2022-12-10 15:55:12 -0800

    started work on the new startscreen assests/class

---
+++ minimalist-game-framework-core/Game/StartScreen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class StartScreen
+{
+    Texture startScreen = Engine.LoadTexture("startScreen/startScreen.png");
+    Font font = Engine.LoadFont("font.ttf",25);
+    int difficulty = 0;
+    public StartScreen()
+    {
+
+    }
+    public void draw()
+    {
+        Engine.DrawTexture(startScreen, Vector2.Zero, size: Game.Resolution) ;
+
+    }
+    public void Update()
+    {
+
+    }
+    public int GetDifficulty()
+    {
+        return difficulty;
+    }
+}
